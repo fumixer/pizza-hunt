7d3a6e5af5e857e8af1817aa8ef63de36085168d
fumixer <fumixer@gmail.com>
2022-08-27 17:22:16 -0400

    controllers

--- Models/Pizza.js
+++ Models/Pizza.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require('mongoose');
+const Pizza = require('./Pizza');
+const Comment = require('./Comment');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
       type: String
     },
@@ -9,17 +12,44 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     size: {
       type: String,
       default: 'Large'
     },
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
 
   // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+comments: [
+  {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+  }
+]
+
 // export the Pizza model
-module.exports = Pizza;
+// module.exports = Pizza;
+
+module.exports = { Pizza, Comment };
